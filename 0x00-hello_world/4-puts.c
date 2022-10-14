a56397da11d7b6e8d1184241ccb87a549b54c8e8
adeleke12345 <segun_olapade@yahoo.com>
2022-10-13 20:44:43 -0700

    first commit in C lang

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+/*****
+ * This is main - Entry point
+ * The return Keyword Word: Always 0 for (Success)
+ *
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ *This is the main - Entry point
+ *It always return: 0 for (Successful) execution
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
